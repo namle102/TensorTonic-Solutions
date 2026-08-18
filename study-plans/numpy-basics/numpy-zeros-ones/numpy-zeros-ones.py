1064c2e6d78e6f1a2319a39f2bce581e99203251
Nam Le <139657367+namle102@users.noreply.github.com>
2026-08-18 13:42:15 -0400

    Solve numpy-zeros-ones

--- study-plans/numpy-basics/numpy-zeros-ones/numpy-zeros-ones.py
+++ study-plans/numpy-basics/numpy-zeros-ones/numpy-zeros-ones.py
@@ -4,7 +4,9 @@ def create_filled_array(shape, kind):
     """
     Returns: 2D numpy array of given shape with dtype float64
     """
-    row, col = shape[0], shape[1]
     if kind == 'zeros':
-        return np.zeros((row, col))
-    return np.ones((row, col))
+        return np.zeros(shape, dtype=np.float64)
+    elif kind == 'ones':
+        return np.ones(shape, dtype=np.float64)
+    else:
+        raise ValueError(f'Unknown kind: {kind}')
