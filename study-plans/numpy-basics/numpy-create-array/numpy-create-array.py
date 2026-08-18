e69257d6be75aec8dfc75a9b0209ff68103f7c0f
Nam Le <139657367+namle102@users.noreply.github.com>
2026-08-18 13:24:05 -0400

    Solve numpy-create-array

--- study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
+++ study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
@@ -4,4 +4,4 @@ def create_array(data):
     """
     Returns: 2D numpy array with dtype float64
     """
-    return np.asarray(data, dtype=np.float64)
+    return np.atleast_2d(np.asarray(data, dtype=np.float64))
