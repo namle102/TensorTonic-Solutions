7f11c19aabb883eba53cb04fbca85a109c40e2ff
Nam Le <139657367+namle102@users.noreply.github.com>
2026-08-20 23:24:11 -0400

    Solve numpy-fancy-indexing

---
+++ study-plans/numpy-basics/numpy-fancy-indexing/numpy-fancy-indexing.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def select_by_index(arr, indices, axis):
+    """
+    Returns: 2D ndarray of float64
+    """
+    a = np.array(arr, dtype=np.float64)
+    if axis == 0:
+        return a[indices, :]
+    else:
+        return a[:, indices]
