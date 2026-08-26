21514b3b2c507e130f4b4011a26e2cba5777f915
Nam Le <139657367+namle102@users.noreply.github.com>
2026-08-26 13:49:22 -0400

    Solve ml-logistic-regression

--- study-plans/cracking-ml/ml-logistic-regression/ml-logistic-regression.py
+++ study-plans/cracking-ml/ml-logistic-regression/ml-logistic-regression.py
@@ -12,11 +12,11 @@ def logistic_regression(X, y, lr=0.01, n_iters=1000):
 
     for _ in range(n_iters):
         z = X@w + b
-        y_hat = 1 / (1 + np.exp(-z))
+        y_hat = 1.0 / (1.0 + np.exp(-z))
         err = y_hat - y
-        dw = (1/n) * (X.T @ err)
-        db = (1/n) * np.sum(err)
+        dw = (1.0/n) * (X.T @ err)
+        db = (1.0/n) * np.sum(err)
         w -= lr * dw
         b -= lr * db
 
-    return (w, b)
+    return (w.tolist(), float(b))
